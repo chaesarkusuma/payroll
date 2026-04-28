2bf27d81ea2f974b984cccd15b6ccefe9cac1e85
Chaesar Kusuma <chaesarputra14@gmail.com>
2026-04-28 11:32:56 +0700

    first commit

---
+++ app/Filament/Resources/Schedules/Schemas/ScheduleForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Schedules\Schemas;
+
+use App\Models\User;
+use Filament\Forms\Components\Select;
+use Filament\Schemas\Schema;
+
+class ScheduleForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('user_id')
+                    ->label('Nama Karyawan')
+                    ->searchable()
+                    ->options(User::query()->pluck('name', 'id'))
+                    ->required(),
+                Select::make('shift_id')
+                    ->relationship('shift', 'name')
+                    ->required(),
+                Select::make('office_id')
+                    ->relationship('office', 'name')
+                    ->required(),
+            ]);
+    }
+}
