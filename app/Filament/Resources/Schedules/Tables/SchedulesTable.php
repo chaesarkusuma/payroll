7785f5866b9df766111ba98c5c4e2fb5419f4b7d
Chaesar Kusuma <chaesarputra14@gmail.com>
2026-04-30 11:09:05 +0700

    Redesign UI

--- app/Filament/Resources/Schedules/Tables/SchedulesTable.php
+++ app/Filament/Resources/Schedules/Tables/SchedulesTable.php
@@ -2,10 +2,12 @@
 
 namespace App\Filament\Resources\Schedules\Tables;
 
+use App\Models\Schedule;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\BooleanColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -16,8 +18,12 @@ class SchedulesTable
         return $table
             ->columns([
                 TextColumn::make('user.name')
+                    ->label('Nama Pegawai')
                     ->searchable(),
+                BooleanColumn::make('is_wfa')
+                    ->label('WFA'),
                 TextColumn::make('shift.name')
+                    ->description(fn (Schedule $record) => $record->shift->start_time . ' - ' . $record->shift->end_time)
                     ->searchable(),
                 TextColumn::make('office.name')
                     ->searchable(),
--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Users\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Facades\Hash;
 
@@ -14,22 +16,30 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                Select::make('roles.name')
-                    ->relationship('roles', 'name')
-                    ->preload()
-                    ->searchable(),
-                DateTimePicker::make('email_verified_at'),
-                TextInput::make('password')
-                    ->password()
-                    ->dehydrateStateUsing(fn ($state) => Hash::make($state))
-                    ->dehydrated(fn ($state) => filled($state))
-                    ->required(fn (string $context): bool => $context === 'create'),
+                Group::make()->components([
+                    Section::make()->components([
+                        TextInput::make('name')
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
+                            ->required(),
+                        Select::make('roles.name')
+                            ->relationship('roles', 'name')
+                            ->preload()
+                            ->searchable(), 
+                    ])
+                ]),
+                Group::make()->components([
+                    Section::make()->components([
+                        DateTimePicker::make('email_verified_at'),
+                        TextInput::make('password')
+                            ->password()
+                            ->dehydrateStateUsing(fn ($state) => Hash::make($state))
+                            ->dehydrated(fn ($state) => filled($state))
+                            ->required(fn (string $context): bool => $context === 'create'),
+                    ])
+                ]),
             ]);
     }
 }
